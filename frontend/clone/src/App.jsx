7a0917650d30a5739fc17ada1484307e7728a217
Hisham0mohiuddin <hishamjuly@gmail.com>
2026-07-17 09:09:56 +0300

    made the base folder config with tailwind css

---
+++ frontend/clone/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      lets build twitter clone 
+    </div>
+  ) 
+}
+
+export default App
